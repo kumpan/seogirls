52874af9f2c7621034774f2785e5e4e8308f9dbd
Per Riekkola <per@webbochmer.se>
2020-04-17 10:50:38 +0200

    Added GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -195,7 +195,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // edit below
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-153761209-1`,
       },
     },
     {
